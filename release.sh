cb1e6410562f34d40b19713c25e3ae6bccbe819a
Raghu Nambiath <a0850439@gtlnx01.gt.design.ti.com>
2010-11-14 13:43:35 -0500

    release.sh:Added version suffix in  tar balls. iblcfg.h: Removed extra CR setupenvLnx.sh: Modified path for tools

--- release.sh
+++ release.sh
@@ -1,53 +1,53 @@
 #!/bin/sh
 # Generate the source release tarballs
+export IBL_VERSION="0_5_1"
 cd ../
-if [ -f ibl_src.tar ]; then rm ibl_src.tar; fi
+if [ -f ibl_src_$IBL_VERSION.tar ]; then rm ibl_src_$IBL_VERSION.tar; fi
 
-tar -cvf ibl_src.tar ibl/src ibl/doc
+tar -cvf ibl_src_$IBL_VERSION.tar ibl/src ibl/doc
 
 cd ibl
 # Create directories for binary builds
-if [ -d ibl_bin ]; then rm -r ibl_bin; fi
+if [ -d ibl_bin_$IBL_VERSION ]; then rm -r ibl_bin_$IBL_VERSION; fi
 
-mkdir ibl_bin
-mkdir ibl_bin/c6455/
-mkdir ibl_bin/c6455/be
-mkdir ibl_bin/c6455/le
+mkdir ibl_bin_$IBL_VERSION
+mkdir ibl_bin_$IBL_VERSION/c6455/
+mkdir ibl_bin_$IBL_VERSION/c6455/be
+mkdir ibl_bin_$IBL_VERSION/c6455/le
 
-mkdir ibl_bin/c6472/
-mkdir ibl_bin/c6472/be
-mkdir ibl_bin/c6472/le
+mkdir ibl_bin_$IBL_VERSION/c6472/
+mkdir ibl_bin_$IBL_VERSION/c6472/be
+mkdir ibl_bin_$IBL_VERSION/c6472/le
 
-mkdir ibl_bin/c6474/
-mkdir ibl_bin/c6474/be
-mkdir ibl_bin/c6474/le
+mkdir ibl_bin_$IBL_VERSION/c6474/
+mkdir ibl_bin_$IBL_VERSION/c6474/be
+mkdir ibl_bin_$IBL_VERSION/c6474/le
 
-#mkdir ibl_bin/c6474l/
-#mkdir ibl_bin/c6474l/be
-#mkdir ibl_bin/c6474l/le
+#mkdir ibl_bin_$IBL_VERSION/c6474l/
+#mkdir ibl_bin_$IBL_VERSION/c6474l/be
+#mkdir ibl_bin_$IBL_VERSION/c6474l/le
 
-#mkdir ibl_bin/c6457/
-#mkdir ibl_bin/c6457/be
-#mkdir ibl_bin/c6457/le
+#mkdir ibl_bin_$IBL_VERSION/c6457/
+#mkdir ibl_bin_$IBL_VERSION/c6457/be
+#mkdir ibl_bin_$IBL_VERSION/c6457/le
 
-# Overwrite Linux environment file
-cp -f setupenvLnx.sh src/make/setupenvLnx.sh
+
+# Set Build environment
+source setupenvLnx.sh
 
 # Build C6455 LE
 pushd src/make
-# Set Build environment
-source setupenvLnx.sh
 make clean
 make c6455 ENDIAN=little
 popd
 # Copy the files to the Binary package
-cp -f src/make/ibl_c6455/i2crom_le.dat ibl_bin/c6455/le/
-cp -f src/util/i2cWrite/i2cWrite.out   ibl_bin/c6455/le/
-cp -f src/util/i2cConfig/i2cparam_c6455.out   ibl_bin/c6455/le/
+cp -f src/make/ibl_c6455/i2crom_le.dat ibl_bin_$IBL_VERSION/c6455/le/
+cp -f src/util/i2cWrite/i2cWrite.out   ibl_bin_$IBL_VERSION/c6455/le/
+cp -f src/util/i2cConfig/i2cparam_c6455.out   ibl_bin_$IBL_VERSION/c6455/le/
 
 
 # Copy i2cConfig GEL file
-cp -f src/util/i2cConfig/i2cConfig.gel ibl_bin/i2cConfig.gel
+cp -f src/util/i2cConfig/i2cConfig.gel ibl_bin_$IBL_VERSION/i2cConfig.gel
 
 
 # Build C6455 BE
@@ -56,9 +56,9 @@ make clean
 make c6455 ENDIAN=big
 popd
 # Copy the files to the Binary package
-cp -f src/make/ibl_c6455/i2crom_be.dat ibl_bin/c6455/be/
-cp -f src/util/i2cWrite/i2cWrite.out   ibl_bin/c6455/be/
-cp -f src/util/i2cConfig/i2cparam_c6455.out   ibl_bin/c6455/be/
+cp -f src/make/ibl_c6455/i2crom_be.dat ibl_bin_$IBL_VERSION/c6455/be/
+cp -f src/util/i2cWrite/i2cWrite.out   ibl_bin_$IBL_VERSION/c6455/be/
+cp -f src/util/i2cConfig/i2cparam_c6455.out   ibl_bin_$IBL_VERSION/c6455/be/
 
 # Build C6472 le
 pushd src/make
@@ -66,9 +66,9 @@ make clean
 make c6472 ENDIAN=little
 popd
 # Copy the files to the Binary package
-cp -f src/make/ibl_c6472/i2crom_le.dat ibl_bin/c6472/le/
-cp -f src/util/i2cWrite/i2cWrite.out   ibl_bin/c6472/le/
-cp -f src/util/i2cConfig/i2cparam_c6472.out   ibl_bin/c6472/le/
+cp -f src/make/ibl_c6472/i2crom_le.dat ibl_bin_$IBL_VERSION/c6472/le/
+cp -f src/util/i2cWrite/i2cWrite.out   ibl_bin_$IBL_VERSION/c6472/le/
+cp -f src/util/i2cConfig/i2cparam_c6472.out   ibl_bin_$IBL_VERSION/c6472/le/
 
 
 # Build c6472 be
@@ -77,9 +77,9 @@ make clean
 make c6472 ENDIAN=big
 popd
 # Copy the files to the Binary package
-cp -f src/make/ibl_c6472/i2crom_be.dat ibl_bin/c6472/be/
-cp -f src/util/i2cWrite/i2cWrite.out   ibl_bin/c6472/be/
-cp -f src/util/i2cConfig/i2cparam_c6472.out   ibl_bin/c6472/be/
+cp -f src/make/ibl_c6472/i2crom_be.dat ibl_bin_$IBL_VERSION/c6472/be/
+cp -f src/util/i2cWrite/i2cWrite.out   ibl_bin_$IBL_VERSION/c6472/be/
+cp -f src/util/i2cConfig/i2cparam_c6472.out   ibl_bin_$IBL_VERSION/c6472/be/
 
 # Build c6474 le
 pushd src/make
@@ -87,9 +87,9 @@ make clean
 make c6474 ENDIAN=little
 popd
 # Copy the files to the Binary package
-cp -f src/make/ibl_c6474/i2crom_le.dat ibl_bin/c6474/le/
-cp -f src/util/i2cWrite/i2cWrite.out   ibl_bin/c6474/le/
-cp -f src/util/i2cConfig/i2cparam_c6474.out   ibl_bin/c6474/le/
+cp -f src/make/ibl_c6474/i2crom_le.dat ibl_bin_$IBL_VERSION/c6474/le/
+cp -f src/util/i2cWrite/i2cWrite.out   ibl_bin_$IBL_VERSION/c6474/le/
+cp -f src/util/i2cConfig/i2cparam_c6474.out   ibl_bin_$IBL_VERSION/c6474/le/
 
 
 # Build c6474 be
@@ -98,9 +98,9 @@ make clean
 make c6474 ENDIAN=big
 popd
 # Copy the files to the Binary package
-cp -f src/make/ibl_c6474/i2crom_be.dat ibl_bin/c6474/be/
-cp -f src/util/i2cWrite/i2cWrite.out   ibl_bin/c6474/be/
-cp -f src/util/i2cConfig/i2cparam_c6474.out   ibl_bin/c6474/be/
+cp -f src/make/ibl_c6474/i2crom_be.dat ibl_bin_$IBL_VERSION/c6474/be/
+cp -f src/util/i2cWrite/i2cWrite.out   ibl_bin_$IBL_VERSION/c6474/be/
+cp -f src/util/i2cConfig/i2cparam_c6474.out   ibl_bin_$IBL_VERSION/c6474/be/
 
 # Build c6474l le
 #pushd src/make
@@ -108,9 +108,9 @@ cp -f src/util/i2cConfig/i2cparam_c6474.out   ibl_bin/c6474/be/
 #make c6474l ENDIAN=little
 #popd
 # Copy the files to the Binary package
-#cp -f src/make/ibl_c6474l/i2crom_le.dat ibl_bin/c6474l/le/
-#cp -f src/util/i2cWrite/i2cWrite.out   ibl_bin/c6474l/le/
-#cp -f src/util/i2cConfig/i2cparam_c6474l.out   ibl_bin/c6474l/le/
+#cp -f src/make/ibl_c6474l/i2crom_le.dat ibl_bin_$IBL_VERSION/c6474l/le/
+#cp -f src/util/i2cWrite/i2cWrite.out   ibl_bin_$IBL_VERSION/c6474l/le/
+#cp -f src/util/i2cConfig/i2cparam_c6474l.out   ibl_bin_$IBL_VERSION/c6474l/le/
 
 
 # Build c6474l be
@@ -119,9 +119,9 @@ cp -f src/util/i2cConfig/i2cparam_c6474.out   ibl_bin/c6474/be/
 #make c6474l ENDIAN=big
 #popd
 # Copy the files to the Binary package
-#cp -f src/make/ibl_c6474l/i2crom_be.dat ibl_bin/c6474l/be/
-#cp -f src/util/i2cWrite/i2cWrite.out   ibl_bin/c6474l/be/
-#cp -f src/util/i2cConfig/i2cparam_c6474l.out   ibl_bin/c6474l/be/
+#cp -f src/make/ibl_c6474l/i2crom_be.dat ibl_bin_$IBL_VERSION/c6474l/be/
+#cp -f src/util/i2cWrite/i2cWrite.out   ibl_bin_$IBL_VERSION/c6474l/be/
+#cp -f src/util/i2cConfig/i2cparam_c6474l.out   ibl_bin_$IBL_VERSION/c6474l/be/
 
 
 # Build c6457 le
@@ -130,9 +130,9 @@ cp -f src/util/i2cConfig/i2cparam_c6474.out   ibl_bin/c6474/be/
 #make c6457 ENDIAN=little
 #popd
 # Copy the files to the Binary package
-#cp -f src/make/ibl_c6457/i2crom_le.dat ibl_bin/c6457/le/
-#cp -f src/util/i2cWrite/i2cWrite.out   ibl_bin/c6457/le/
-#cp -f src/util/i2cConfig/i2cparam_c6457.out   ibl_bin/c6457/le/
+#cp -f src/make/ibl_c6457/i2crom_le.dat ibl_bin_$IBL_VERSION/c6457/le/
+#cp -f src/util/i2cWrite/i2cWrite.out   ibl_bin_$IBL_VERSION/c6457/le/
+#cp -f src/util/i2cConfig/i2cparam_c6457.out   ibl_bin_$IBL_VERSION/c6457/le/
 
 
 # Build c6457 be
@@ -141,10 +141,10 @@ cp -f src/util/i2cConfig/i2cparam_c6474.out   ibl_bin/c6474/be/
 #make c6457 ENDIAN=big
 #popd
 # Copy the files to the Binary package
-#cp -f src/make/ibl_c6457/i2crom_be.dat ibl_bin/c6457/be/
-#cp -f src/util/i2cWrite/i2cWrite.out   ibl_bin/c6457/be/
-#cp -f src/util/i2cConfig/i2cparam_c6457.out   ibl_bin/c6457/be/
+#cp -f src/make/ibl_c6457/i2crom_be.dat ibl_bin_$IBL_VERSION/c6457/be/
+#cp -f src/util/i2cWrite/i2cWrite.out   ibl_bin_$IBL_VERSION/c6457/be/
+#cp -f src/util/i2cConfig/i2cparam_c6457.out   ibl_bin_$IBL_VERSION/c6457/be/
 
 # Create Tar archive for binary package
-tar -cvzf ibl_bin.tar ibl_bin
+tar -cvzf ibl_bin_$IBL_VERSION.tar ibl_bin_$IBL_VERSION
 
--- src/make/setupenvLnx.sh
+++ src/make/setupenvLnx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export OS="Linux"
-export C6X_BASE_DIR="/sim/scratch_a0216561/opt/TI_CGT_C6000_7.2.0A10218"
+export C6X_BASE_DIR="/opt/TI_CGT_C6000_7.2.0A10218"
 export PATH="$C6X_BASE_DIR/bin:$PATH"
-export TOOLSC6X="/sim/scratch_a0216561/opt/TI_CGT_C6000_7.2.0A10218"
-export TOOLSC6XDOS="/sim/scratch_a0216561/opt/TI_CGT_C6000_7.2.0A10218"
-export TOOLSBIOSC6XDOS="/sim/scratch_a0216561/opt/TI_CGT_C6000_7.2.0A10218"
+export TOOLSC6X="/opt/TI_CGT_C6000_7.2.0A10218"
+export TOOLSC6XDOS="/opt/TI_CGT_C6000_7.2.0A10218"
+export TOOLSBIOSC6XDOS="/opt/TI_CGT_C6000_7.2.0A10218"
