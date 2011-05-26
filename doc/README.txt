ebf375e7b60ca515666f549735468728d6680326
Sandeep Nair <sandeep_n@ti.com>
2011-05-26 18:59:40 -0400

    Documentation updated for IR's SDOCM00080092 & SDOCM00081107

--- doc/README.txt
+++ doc/README.txt
@@ -1,8 +1,15 @@
+=====================================================
+C6x Intermediate Bootloader (IBL)
+Version: 1.0.0.5
+May 2011
+=====================================================
+
 This directory contains the following documents:
 
-1. release_info.txt: IBL release Notes
+1. build_instructions.txt: IBL Build instructions
 2. IBL-software-manifest.html: Software manifest
-3. IBL-Configuration: Folder containing documentation on IBL configuration
+3. IBL-Configuration: Folder containing documentation on IBL configuration.
+                      Load the index.html file from this folder in a browser.
 4. evmc6678-instructions.txt: Document describing steps to use IBL on the c6678 EVM
 5. evmc6670-instructions.txt: Document describing steps to use IBL on the c6670 EVM
 
--- release.sh
+++ release.sh
@@ -47,9 +47,9 @@ make clean
 make evm_c6455 I2C_MAP_ADDR=0x500
 popd
 # Copy the files to the Binary package
-cp -f src/make/ibl_c6455/i2crom.dat ibl_bin_$IBL_VERSION/c6455/le-be/i2crom_0x50_c6455.dat
-cp -f src/make/ibl_c6455/i2crom.bin ibl_bin_$IBL_VERSION/c6455/le-be/i2crom_0x50_c6455.bin
-cp -f src/util/i2cConfig/i2cparam_c6455_le.out ibl_bin_$IBL_VERSION/c6455/le-be/i2cparam_c6455_le_0x500.out
+cp -f src/make/bin/i2crom_0x50_c6455_le-be.dat ibl_bin_$IBL_VERSION/c6455/le-be/
+cp -f src/make/bin/i2crom_0x50_c6455_le-be.bin ibl_bin_$IBL_VERSION/c6455/le-be/
+cp -f src/make/bin/i2cparam_0x50_c6455_le_0x500.out ibl_bin_$IBL_VERSION/c6455/le-be/
 
 # Build C6455 EVM BE
 pushd src/make
@@ -57,7 +57,7 @@ make clean
 make evm_c6455 I2C_MAP_ADDR=0x800
 popd
 # Copy the files to the Binary package
-cp -f src/util/i2cConfig/i2cparam_c6455_le.out ibl_bin_$IBL_VERSION/c6455/le-be/i2cparam_c6455_le_0x800.out
+cp -f src/make/bin/i2cparam_0x50_c6455_be_0x800.out ibl_bin_$IBL_VERSION/c6455/le-be/
 
 
 # Build C6472 le
@@ -66,9 +66,9 @@ make clean
 make evm_c6472 I2C_MAP_ADDR=0x500
 popd
 # Copy the files to the Binary package
-cp -f src/make/ibl_c6472/i2crom.dat ibl_bin_$IBL_VERSION/c6472/le-be/i2crom_0x50_c6472.dat
-cp -f src/make/ibl_c6472/i2crom.bin ibl_bin_$IBL_VERSION/c6472/le-be/i2crom_0x50_c6472.bin
-cp -f src/util/i2cConfig/i2cparam_c6472_le.out ibl_bin_$IBL_VERSION/c6472/le-be/i2cparam_c6472_le_0x500.out
+cp -f src/make/bin/i2crom_0x50_c6472_le-be.dat ibl_bin_$IBL_VERSION/c6472/le-be/
+cp -f src/make/bin/i2crom_0x50_c6472_le-be.bin ibl_bin_$IBL_VERSION/c6472/le-be/
+cp -f src/make/bin/i2cparam_0x50_c6472_le_0x500.out ibl_bin_$IBL_VERSION/c6472/le-be/
 
 # Build C6472 EVM BE
 pushd src/make
@@ -76,68 +76,84 @@ make clean
 make evm_c6472 I2C_MAP_ADDR=0x800
 popd
 # Copy the files to the Binary package
-cp -f src/util/i2cConfig/i2cparam_c6472_le.out ibl_bin_$IBL_VERSION/c6472/le-be/i2cparam_c6472_le_0x800.out
+cp -f src/make/bin/i2cparam_0x50_c6472_be_0x800.out ibl_bin_$IBL_VERSION/c6472/le-be/
 
-# Build c6474 EVM
+# Build c6474 EVM little endian
 pushd src/make
 make clean
-make evm_c6474 
+make evm_c6474 ENDIAN=little
 popd
 # Copy the files to the Binary package
-cp -f src/make/ibl_c6474/i2crom_0x50_c6474_le.dat ibl_bin_$IBL_VERSION/c6474/le/
-cp -f src/make/ibl_c6474/i2crom_0x50_c6474_le.bin ibl_bin_$IBL_VERSION/c6474/le/
-cp -f src/make/ibl_c6474/i2crom_0x50_c6474_be.dat ibl_bin_$IBL_VERSION/c6474/be/
-cp -f src/make/ibl_c6474/i2crom_0x50_c6474_be.bin ibl_bin_$IBL_VERSION/c6474/be/
-cp -f src/util/i2cConfig/i2cparam_c6474_le.out   ibl_bin_$IBL_VERSION/c6474/le/
-cp -f src/util/i2cConfig/i2cparam_c6474_be.out   ibl_bin_$IBL_VERSION/c6474/be/
+cp -f src/make/bin/i2crom_0x50_c6474_le.dat ibl_bin_$IBL_VERSION/c6474/le/
+cp -f src/make/bin/i2crom_0x50_c6474_le.bin ibl_bin_$IBL_VERSION/c6474/le/
+cp -f src/make/bin/i2cparam_0x50_c6474_le_0x200.out ibl_bin_$IBL_VERSION/c6474/le/
 
 
-# Build c6474l EVM
+# Build c6474 EVM Big endian
 pushd src/make
 make clean
-make evm_c6474l
+make evm_c6474 ENDIAN=big
 popd
 # Copy the files to the Binary package
-cp -f src/make/ibl_c6474/i2crom_0x50_c6474l_le.dat ibl_bin_$IBL_VERSION/c6474l/le/
-cp -f src/make/ibl_c6474/i2crom_0x50_c6474l_le.bin ibl_bin_$IBL_VERSION/c6474l/le/
-cp -f src/make/ibl_c6474/i2crom_0x50_c6474l_be.dat ibl_bin_$IBL_VERSION/c6474l/be/
-cp -f src/make/ibl_c6474/i2crom_0x50_c6474l_be.bin ibl_bin_$IBL_VERSION/c6474l/be/
-cp -f src/util/i2cConfig/i2cparam_c6474_le.out   ibl_bin_$IBL_VERSION/c6474l/le/i2cparam_c6474l_le.out
-cp -f src/util/i2cConfig/i2cparam_c6474_be.out   ibl_bin_$IBL_VERSION/c6474l/be/i2cparam_c6474l_be.out
+cp -f src/make/bin/i2crom_0x50_c6474_be.dat ibl_bin_$IBL_VERSION/c6474/be/
+cp -f src/make/bin/i2crom_0x50_c6474_be.bin ibl_bin_$IBL_VERSION/c6474/be/
+cp -f src/make/bin/i2cparam_0x50_c6474_be_0x200.out ibl_bin_$IBL_VERSION/c6474/be/
 
-
-# Build c6457 EVM 
+# Build c6474l EVM little Endian
 pushd src/make
 make clean
-make evm_c6457 
+make evm_c6474l ENDIAN=little
 popd
-cp -f src/make/ibl_c6457/i2crom_0x50_c6457_le.dat ibl_bin_$IBL_VERSION/c6457/le/
-cp -f src/make/ibl_c6457/i2crom_0x50_c6457_le.bin ibl_bin_$IBL_VERSION/c6457/le/
-cp -f src/make/ibl_c6457/i2crom_0x50_c6457_be.dat ibl_bin_$IBL_VERSION/c6457/be/
-cp -f src/make/ibl_c6457/i2crom_0x50_c6457_be.bin ibl_bin_$IBL_VERSION/c6457/be/
-cp -f src/util/i2cConfig/i2cparam_c6457_le.out   ibl_bin_$IBL_VERSION/c6457/le/
-cp -f src/util/i2cConfig/i2cparam_c6457_be.out   ibl_bin_$IBL_VERSION/c6457/be/
+# Copy the files to the Binary package
+cp -f src/make/bin/i2crom_0x50_c6474l_le.dat ibl_bin_$IBL_VERSION/c6474l/le/
+cp -f src/make/bin/i2crom_0x50_c6474l_le.bin ibl_bin_$IBL_VERSION/c6474l/le/
+cp -f src/make/bin/i2cparam_0x50_c6474l_le_0x200.out ibl_bin_$IBL_VERSION/c6474l/le/
+
+# Build c6474l EVM Big Endian
+pushd src/make
+make clean
+make evm_c6474l ENDIAN=big
+popd
+# Copy the files to the Binary package
+cp -f src/make/bin/i2crom_0x50_c6474l_be.dat ibl_bin_$IBL_VERSION/c6474l/be/
+cp -f src/make/bin/i2crom_0x50_c6474l_be.bin ibl_bin_$IBL_VERSION/c6474l/be/
+cp -f src/make/bin/i2cparam_0x50_c6474l_be_0x200.out ibl_bin_$IBL_VERSION/c6474l/be/
+
+# Build c6457 EVM little
+pushd src/make
+make clean
+make evm_c6457 ENDIAN=little
+popd
+cp -f src/make/bin/i2crom_0x50_c6457_le.dat ibl_bin_$IBL_VERSION/c6457/le/
+cp -f src/make/bin/i2crom_0x50_c6457_le.bin ibl_bin_$IBL_VERSION/c6457/le/
+cp -f src/make/bin/i2cparam_0x50_c6457_le_0x200.out ibl_bin_$IBL_VERSION/c6457/le/
+
+# Build c6457 EVM big
+pushd src/make
+make clean
+make evm_c6457 ENDIAN=big
+popd
+cp -f src/make/bin/i2crom_0x50_c6457_be.dat ibl_bin_$IBL_VERSION/c6457/be/
+cp -f src/make/bin/i2crom_0x50_c6457_be.bin ibl_bin_$IBL_VERSION/c6457/be/
+cp -f src/make/bin/i2cparam_0x50_c6457_be_0x200.out ibl_bin_$IBL_VERSION/c6457/be/
 
 # Build c6678 EVM 
 pushd src/make
 make clean
 make evm_c6678_i2c ENDIAN=little I2C_BUS_ADDR=0x51 
 popd
-cp -f src/make/ibl_c661x/i2crom.dat ibl_bin_$IBL_VERSION/c6678/le/i2crom_0x51_c6678_le.dat
-cp -f src/make/ibl_c661x/i2crom.bin ibl_bin_$IBL_VERSION/c6678/le/i2crom_0x51_c6678_le.bin
-cp -f src/util/i2cConfig/i2cparam_c661x_le.out ibl_bin_$IBL_VERSION/c6678/le/i2cparam_c6678_le.out
-cp -f src/make/ibl_c661x/i2crom.dat ibl_bin_$IBL_VERSION/c6670/le/i2crom_0x51_c6670_le.dat
-cp -f src/make/ibl_c661x/i2crom.bin ibl_bin_$IBL_VERSION/c6670/le/i2crom_0x51_c6670_le.bin
-cp -f src/util/i2cConfig/i2cparam_c661x_le.out ibl_bin_$IBL_VERSION/c6670/le/i2cparam_c6670_le.out
+cp -f src/make/bin/i2crom_0x51_c6678_le.dat ibl_bin_$IBL_VERSION/c6678/le/
+cp -f src/make/bin/i2crom_0x51_c6678_le.bin ibl_bin_$IBL_VERSION/c6678/le/
+cp -f src/make/bin/i2cparam_0x51_c6678_le_0x500.out ibl_bin_$IBL_VERSION/c6678/le/
 
 # Build c6670 EVM 
 pushd src/make
 make clean
 make evm_c6670_i2c ENDIAN=little I2C_BUS_ADDR=0x51 
 popd
-cp -f src/make/ibl_c661x/i2crom.dat ibl_bin_$IBL_VERSION/c6670/le/i2crom_0x51_c6670_le.dat
-cp -f src/make/ibl_c661x/i2crom.bin ibl_bin_$IBL_VERSION/c6670/le/i2crom_0x51_c6670_le.bin
-cp -f src/util/i2cConfig/i2cparam_c661x_le.out ibl_bin_$IBL_VERSION/c6670/le/i2cparam_c6670_le.out
+cp -f src/make/bin/i2crom_0x51_c6670_le.dat ibl_bin_$IBL_VERSION/c6670/le/
+cp -f src/make/bin/i2crom_0x51_c6670_le.bin ibl_bin_$IBL_VERSION/c6670/le/
+cp -f src/make/bin/i2cparam_0x51_c6670_le_0x500.out ibl_bin_$IBL_VERSION/c6670/le/
 
 # Create Tar archive for binary package
 tar -czf ibl_bin_$IBL_VERSION.tgz ibl_bin_$IBL_VERSION
