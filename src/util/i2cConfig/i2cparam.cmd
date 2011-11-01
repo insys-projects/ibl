14580f8dce02c49ba03273a34e8e29ad929da05f
Sandeep Paulraj <s-paulraj@ti.com>
2011-11-01 17:06:34 -0400

    Minor Update to i2c param cmd file
    Signed-off-by: Sandeep Paulraj <s-paulraj@ti.com>

--- src/util/i2cConfig/i2cparam.cmd
+++ src/util/i2cConfig/i2cparam.cmd
@@ -14,8 +14,8 @@
 MEMORY
 {
    STACK   :  origin = 0x810000 length = 0x0400
-   TEXT    :  origin = 0x810400 length = 0x9000
-   DATA    :  origin = 0x819400 length = 0x0800
+   TEXT    :  origin = 0x810400 length = 0x8f00
+   DATA    :  origin = 0x819300 length = 0x0900
    SYSMEM  :  origin = 0x80a000 length = 0x0800
 }
 
