7aec71711b10172778274f040d2685349d9b772b
dor <do.insys@gmail.com>
2014-06-26 16:44:00 +0400

    Copy IBL binary file into top level bin directory

--- src/util/iblConfig/build/iblconfig.sh
+++ src/util/iblConfig/build/iblconfig.sh
@@ -10,6 +10,7 @@ echo "-------- Build for BOARD: ${INSYS_BOARD} --- I2C : ${I2C_BUS_ADDRESS} ----
 if [ ${INSYS_BOARD} == INSYS_PEX_SRIO ] ;
 then
 cp ibl.bin ibl_c6678_pex_srio_${I2C_BUS_ADDRESS}.bin;
+cp ibl_c6678_pex_srio_${I2C_BUS_ADDRESS}.bin ../../../../bin/
 fi
 
 
