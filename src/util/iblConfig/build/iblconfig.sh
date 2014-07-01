422029533b838882781b898eeebcde6fcc95c2fe
Karakozov Vladimir <karakozov@gmail.com>
2014-07-01 16:47:15 +0400

    Add cpu number in name of output IBL image file

--- src/util/iblConfig/build/iblconfig.sh
+++ src/util/iblConfig/build/iblconfig.sh
@@ -53,19 +53,21 @@ echo "ethBoot-fileName = c6678_"${FNAME_PREFIX}.bin >> input.txt
 if [ ${INSYS_CPU} == "CPU0" ] ;
 then
     echo "ethBoot-ipAddr = 192.168.0.197" >> input.txt
+    CPUNAME_PREFIX="cpu0"
 fi
 
 if [ ${INSYS_CPU} == "CPU1" ] ;
 then
     echo "ethBoot-ipAddr = 192.168.0.198" >> input.txt
+    CPUNAME_PREFIX="cpu1"
 fi
 
 ./iblConfig.out input.txt
 
 echo "-------- Build for BOARD: ${INSYS_BOARD} --- CPU: ${INSYS_CPU} --- I2C : ${I2C_BUS_ADDRESS} --------"
 
-cp ibl.bin ibl_c6678_${FNAME_PREFIX}_${I2C_BUS_ADDRESS}.bin;
-cp ibl_c6678_${FNAME_PREFIX}_${I2C_BUS_ADDRESS}.bin ../../../../bin/
+cp ibl.bin ibl_c6678_${FNAME_PREFIX}_${CPUNAME_PREFIX}_${I2C_BUS_ADDRESS}.bin;
+cp ibl_c6678_${FNAME_PREFIX}_${CPUNAME_PREFIX}_${I2C_BUS_ADDRESS}.bin ../../../../bin/
 
 cp ibl.bin i2crom_${I2C_BUS_ADDRESS}_c6678_le.bin
 cp i2crom_${I2C_BUS_ADDRESS}_c6678_le.bin ../../../../../../product/
